84f1d19adcee878af440cdd8000b171a0c6c7485
Meyyappan <meyyappan721@gmail.com>
2024-09-26 22:30:10 +0530

    initialize springboot and angular structure

---
+++ crud/src/main/java/com/app/crud/controllers/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.app.crud.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.crud.model.Employee;
+import com.app.crud.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	
+}
